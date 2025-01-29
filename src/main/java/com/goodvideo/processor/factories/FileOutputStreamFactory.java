61c5068ac534aea99b169136995f771d5d207729
Daniel Rossato Martini <martinidaniel@hotmail.com>
2025-01-29 18:12:01 -0300

    chore: fix

--- src/main/java/com/goodvideo/processor/factories/FileOutputStreamFactory.java
+++ src/main/java/com/goodvideo/processor/factories/FileOutputStreamFactory.java
@@ -1,8 +1,11 @@
 package com.goodvideo.processor.factories;
 
+import org.springframework.stereotype.Component;
+
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 
+@Component
 public class FileOutputStreamFactory {
 
     public FileOutputStream createFileOutputStream(String file) throws FileNotFoundException {
--- src/main/java/com/goodvideo/processor/factories/ZipOutputStreamFactory.java
+++ src/main/java/com/goodvideo/processor/factories/ZipOutputStreamFactory.java
@@ -1,8 +1,11 @@
 package com.goodvideo.processor.factories;
 
+import org.springframework.stereotype.Component;
+
 import java.io.FileOutputStream;
 import java.util.zip.ZipOutputStream;
 
+@Component
 public class ZipOutputStreamFactory {
 
     public ZipOutputStream createZipOutputStream(FileOutputStream fos) {
